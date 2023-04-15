6055e77158d6889c24a0db8c4a6dd59d6214daa8
Paco Estrada <pacoestrada77@gmail.com>
2023-04-15 11:09:38 +0200

    Version2.0

--- cambioIcono.sh
+++ cambioIcono.sh
@@ -1,31 +1,35 @@
 #!/bin/bash
 
-# Encontrar la ubicación del archivo .desktop de la aplicación con cadena de caracteres coincidentes
-echo "Ingresa una cadena de caracteres que coincida con el nombre de la aplicación:"
-read app_str
+# Establecer la variable de entorno DBUS_SESSION_BUS_ADDRESS
+export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u)/bus
 
-app_file=$(find / -name "*$app_str*.desktop" 2>/dev/null)
+# Seleccionar la aplicación a la que se le quiere cambiar el icono
+app=$(zenity --entry --title="Cambiar icono de aplicación" --text="Introduce el nombre de la aplicación:")
+
+# Encontrar la ubicación del archivo .desktop de la aplicación con cadena de caracteres coincidentes
+app_files=$(find / -name "*$app*.desktop" 2>/dev/null)
+
+if [ -z "$app_files" ]; then
+    zenity --error --title="Error" --text="No se encontró ningún archivo .desktop que coincida con \"$app\"."
+    exit 1
+fi
+
+# Ofrecer las opciones de aplicaciones encontradas
+app_file=$(zenity --list --title="Selecciona la aplicación" --text="Se encontraron varias aplicaciones que coinciden con \"$app\".\nSelecciona la aplicación a la que quieres cambiar el icono:" --column="Archivo" $app_files)
 
 if [ -z "$app_file" ]; then
-    echo "No se encontró ningún archivo .desktop que coincida con \"$app_str\"."
     exit 1
 fi
 
 # Obtener el nombre del archivo sin la extensión
 app_name=$(basename "$app_file" .desktop)
 
-# Confirmar que es la aplicación a la que se le quiere cambiar el icono
-echo "¿Deseas cambiar el icono de la aplicación \"$app_name\"? [s/n]"
-read confirm
+# Seleccionar la imagen que se usará como nuevo icono
+icon_path=$(zenity --file-selection --title="Selecciona una imagen" --file-filter="Imágenes (*.png *.jpg *.svg *.ico)")
 
-if [ "$confirm" != "s" ]; then
-    echo "Operación cancelada."
+if [ -z "$icon_path" ]; then
     exit 1
 fi
-
-# Seleccionar la imagen que se usará como nuevo icono
-echo "Selecciona la imagen que quieres usar como nuevo icono:"
-read icon_path
 
 # Copiar la imagen seleccionada a la carpeta de iconos de la aplicación
 cp "$icon_path" "/usr/share/icons/hicolor/scalable/apps/$app_name.svg"
@@ -36,4 +40,4 @@ gtk-update-icon-cache -f -t /usr/share/icons/hicolor
 # Actualizar el archivo .desktop con la nueva ruta del icono
 sed -i "s/^Icon=.*/Icon=$app_name/" "$app_file"
 
-echo "El icono de $app_name ha sido cambiado con éxito."
+zenity --info --title="Cambio de icono exitoso" --text="El icono de \"$app\" ha sido cambiado con éxito."
